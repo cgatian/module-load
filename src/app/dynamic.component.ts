f9a3627c99b0cae30107789488b551ba6eb3ec9f
Chaz Gatian <CharlesGatian@hotmail.com>
2017-04-20 20:11:54 -0400

    init

---
+++ src/app/dynamic.component.ts
@@ -0,0 +1,54 @@
+import { Component, Injector, ViewContainerRef, NgModuleFactory, Input, ComponentFactory } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+@Component({
+  selector: 'dynamic-component-loader',
+  template: ''
+})
+export class DynamicComponentLoader {
+
+  constructor(
+    private injector: Injector,
+    private viewRef: ViewContainerRef,
+    private http: Http,
+  ) { }
+
+  @Input() name: string;
+  @Input() componentName: string;
+
+  ngOnInit() {
+    const metadataPathSuffix = '.plugin-metadata.json';
+    const factoryPathSuffix = 'plugin-factory.umd.js';
+    const pluginUrlPrefix = 'assets/plugins';
+    const factorySuffix = 'NgFactory';
+
+    this.http.get(`${pluginUrlPrefix}/${this.name}/${metadataPathSuffix}`)
+      .map(res => res.json())
+      .map((metadata: PluginMetadata) => {
+
+        const script = document.createElement('script');
+        script.src = `${pluginUrlPrefix}/${this.name}/${factoryPathSuffix}`;
+
+        script.onload = () => {
+
+          const moduleFactory: NgModuleFactory<any> = window[metadata.name][metadata.moduleName + factorySuffix];
+          const moduleRef = moduleFactory.create(this.injector);
+          const factories: Map<any, any> = moduleRef.componentFactoryResolver['_factories'];
+
+          factories.forEach((factory: ComponentFactory<any>) => {
+            if (factory.componentType.name === this.componentName) {
+              this.viewRef.createComponent(factory);
+            }
+          });
+        };
+
+        document.head.appendChild(script);
+
+      }).subscribe();
+  }
+}
+
+export interface PluginMetadata {
+  moduleName: string;
+  name: string;
+}
---
+++ src/plugins/hello-world.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `<div>Hello World!</div>`
+})
+export class HelloWorldComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
